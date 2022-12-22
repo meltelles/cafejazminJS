ecb36ca80761dc6f0eb064c82054e27ab7a9ee5c
Melany Telles <melanytelles@gmail.com>
2022-12-22 20:29:36 -0300

    Segunda entrega del proyecto final

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Café Etiopía 200g",
+    precio: 2000,
+    descuento: 0,
+  },
+  {
+    id: 2,
+    nombre: "Café Colombia 200g",
+    precio: 1400,
+    descuento: 0.15,
+  },
+  {
+    id: 3,
+    nombre: "Café Ecuador 200g",
+    precio: 1900,
+    descuento: 0.1,
+  },
+  {
+    id: 4,
+    nombre: "Café Kenya 200g",
+    precio: 2100,
+    descuento: 0,
+  },
+  {
+    id: 5,
+    nombre: "Café Guatemala 200g",
+    precio: 1500,
+    descuento: 0.2,
+  },
+];
+
+const dtoEnPorcentaje = (descuento) => `${descuento * 100}%`;
+const precioEnPesos = (precio) => `$${precio}`;
+
 alert("Calculadora de precios. Para salir escriba cerrar!");
 
 function calcularDescuento(precio, descuento) {
@@ -15,8 +51,10 @@ function calcularPrecios() {
   let producto;
   let precio;
   let descuento;
+  alert("Calculadora de precios. Para salir escriba: cerrar");
+  let termino = false;
 
-  while (true) {
+  do {
     producto = prompt("Ingrese el nombre del producto: ");
     if (evaluarCerrar(producto)) {
       break;
@@ -49,7 +87,61 @@ function calcularPrecios() {
     const precioCalculado = calcularDescuento(precio, descuento);
 
     alert(`El precio de ${producto} es: $${precioCalculado}`);
+    termino = true;
+  } while (!termino);
+}
+
+function crearProducto(producto) {
+  const productoElem = document.createElement("li");
+  const nombreElem = document.createElement("h4");
+  nombreElem.innerHTML = producto.nombre;
+  const precioElem = document.createElement("p");
+  precioElem.innerHTML = producto.precio;
+  const descuentoElem = document.createElement("p");
+  descuentoElem.innerHTML = producto.descuento;
+  const precioFinalElem = document.createElement("p");
+  precioFinalElem.innerHTML = producto.final;
+
+  productoElem.appendChild(nombreElem);
+  productoElem.appendChild(precioElem);
+  productoElem.appendChild(descuentoElem);
+  productoElem.appendChild(precioFinalElem);
+
+  return productoElem;
+}
+
+function agregarElementos(filtrar) {
+  console.log("funciona");
+  const listaProductos = document.getElementById("productos");
+  listaProductos.innerHTML = "";
+  let productosFiltrados = productos;
+
+  if (filtrar === "sinDescuento") {
+    productosFiltrados = productos.filter((p) => p.descuento === 0);
+  }
+
+  const productosEnTexto = productosFiltrados.map((producto) => {
+    return {
+      nombre: producto.nombre,
+      precio: `precio: ${precioEnPesos(producto.precio)}`,
+      descuento: `descuento: ${dtoEnPorcentaje(producto.descuento)}`,
+      final: `precio final: ${precioEnPesos(
+        calcularDescuento(producto.precio, dtoEnPorcentaje(producto.descuento))
+      )}`,
+    };
+  });
+  console.log(productosEnTexto);
+
+  for (let p of productosEnTexto) {
+    const elem = crearProducto(p);
+    listaProductos.appendChild(elem);
   }
 }
 
-calcularPrecios();
+const boton = document.getElementById("mostrar");
+const botonSinDescuento = document.getElementById("mostrarSinDescuento");
+const botonCalcular = document.getElementById("calcularPrecio");
+
+boton.onclick = agregarElementos;
+botonCalcular.onclick = calcularPrecios;
+botonSinDescuento.onclick = () => agregarElementos("sinDescuento");
